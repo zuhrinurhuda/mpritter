7c741f6a53be6422643274d0da76a4b88c00ae56
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2018-01-18 15:03:04 +0700

    checkpoint 3

--- client/src/components/HomePage.vue
+++ client/src/components/HomePage.vue
@@ -1,12 +1,52 @@
 <template>
+  <div class="ui centered grid container">
+    <div class="ten wide column">
+      <div class="ui form">
+        <div class="field">
+          <textarea rows="5" v-model="tweet"></textarea>
+        </div>
+      </div>
+      <div class="ui segment">
+        <div class="ui comments">  
+          <div class="comment">
+            <a class="avatar">
+              <img src="">
+            </a>
+            <div class="content">
+              <a class="author"></a>
+              <div class="metadata">
+                <div class="date"></div>
+              </div>
+              <div class="text">
+              </div>
+              <div class="actions">
+                <a class="reply">
+                  <i class="comment outline icon"></i>
+                </a>
+                <a class="save">
+                  <i class="retweet icon"></i>
+                </a>
+                <a class="hide">
+                  <i class="empty heart icon"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      tweet: ''
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HomePage from '@/components/HomePage'
+import TimelinePage from '@/components/TimelinePage'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: HomePage
+    },
+    {
+      path: '/timeline',
+      name: 'timeline',
+      component: TimelinePage
     }
   ]
 })
